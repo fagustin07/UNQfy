f96035e67eb0c9b9b27bc36ceda2ee4cf29ab243
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-10-19 02:39:56 -0300

    :fire: rename de server y app
    Signed-off-by: Fede Sandoval <federico.sandoval@alu.unq.edu.ar>

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const rootApp = require('./src/api/app');
-
-const port = 8081;
-
-server = http.createServer(rootApp);
-
-server.listen(port, () => console.log(`Listening on port ${port}`))
