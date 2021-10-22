5016ce38bbc15325c98fc15954809ad0b9fab005
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-10-21 22:19:27 -0300

    :bug: arreglo error handler

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const express  = require('express'); 
+const express = require('express');
 const artists_router = require('./routers/artist_router.js');
 const albums_router = require('./routers/album_router.js');
 const users_router = require('./routers/user_router.js');
@@ -11,13 +11,16 @@ const error_handler = require('./error_handler');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(error_handler);
 
 app.use('/api/artists', artists_router);
 app.use('/api/albums', albums_router);
 app.use('/api/users', users_router);
 app.use('/api/playlists', playlist_router);
 app.use('/api/tracks', track_router);
+
 app.use('*', invalid_router);
+app.use(error_handler);
+
+
 
 module.exports = app;
--- src/errors/basics.js
+++ src/errors/basics.js
@@ -1,36 +1,39 @@
 
 class UNQfyError extends Error {    
-    constructor(msg) {
+    constructor(msg, statusCode, errorCode) {
         super(msg);
+        this.status = statusCode;
+        this.errorCode = errorCode
     }
 }
 
 class BadRequest extends UNQfyError {
     constructor() {
-        super('Bad request!');
+        super('Bad request!', 400, "BAD_REQUEST");
     }
 }
 
 class ResourceAlreadyExists extends UNQfyError {
     constructor(resourceEntity){
-        super(`${resourceEntity} already exists`);
+        super(`${resourceEntity} already exists`, 409, "RESOURCE_ALREADY_EXISTS");
     }
 }
 
 class ResourceNotFound extends UNQfyError {
     constructor(resourceEntity) {
-        super(`${resourceEntity} not found`);
+        super(`${resourceEntity} not found`, 404, "RESOURCE_NOT_FOUND");
     }
 }
 
 
 class RelatedResourceNotFound extends UNQfyError {
     constructor(resourceEntity) {
-        super(`Related ${resourceEntity} not found`);
+        super(`Related ${resourceEntity} not found`, 404, "RELATED_RESOURCE_NOT_FOUND");
     }
 }
 
 module.exports = {
+    UNQfyError,
     BadRequest,
     ResourceAlreadyExists,
     ResourceNotFound,
